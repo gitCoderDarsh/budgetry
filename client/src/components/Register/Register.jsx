b93ea4de4611874911d310ba76316fe9b521df0b
gitCoderDarsh <collegeid3250@gmail.com>
2024-07-30 20:55:32 +0530

    regis design

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import "./Register.css";
 
-function Register(){
+function Register() {
+  const [formData, setFormData] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
 
-    return(
-    <div className="form">
-        <form action="">
-            <input type="text" placeholder="Username" id="userName"/>
-            <input type="email" placeholder="Email" id="email"/>
-            <input type="password" placeholder="Password" id="password"/>
+  const handleChange = (e) => {
+    const {id, value} = e.target;
+    setFormData((prevData) => ({
+        ...prevData,
+        [id]:value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+        const response = await axios.post('http://localhost:3001/api/register', formData);
+        console.log(response.data);
+    }
+    catch(err)
+    {
+        console.error(err);
+    }
+  }
+
+  return (
+    <div className="formHolder">
+      <div className="form">
+        <h1 className="head">Register</h1>
+        <form onClick={handleSubmit}>
+          <input type="text" placeholder="Username" id="userName" onChange={handleChange}/>
+          <br />
+          <input type="email" placeholder="Email" id="email" onChange={handleChange}/>
+          <br />
+          <input type="password" placeholder="Password" id="password" onChange={handleChange}/>
+          <br />
+          <input type="submit" value="Register" id="submit" />
         </form>
+      </div>
     </div>
-    );
+  );
 }
 
 export default Register;
